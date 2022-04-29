37ea513a95cc615a3c45ccbd2797b9fd5494050d
JonathasGit <jonathas-25@hotmail.com>
2022-04-28 21:21:15 -0300

    #1 Rodando um projeto

--- DIO/Program.cs
+++ DIO/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+int numeroDeVezes = 15;
+
+for (int i=0; i < numeroDeVezes; i++) 
+{
+    Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+}
+
 Console.WriteLine("Hello, World!");
